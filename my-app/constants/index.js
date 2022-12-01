2a2217bcce1f22e8efc851aad300fc027fe9e131
Wesoamo500 <sulemanabdulmanan@gmail.com>
2022-12-01 02:30:49 +0000

    today commit

---
+++ my-app/constants/index.js
@@ -0,0 +1,7 @@
+
+export const CRYPTO_DEV_NFT_CONTRACT = "";
+
+export const L3PAD_DAO_CONTRACT = "";
+
+export const CRYPTO_DEV_NFT_ABI= "";
+export const L3PAD_DAO_ABI = "";
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,71 +1,177 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
+import {Contract, providers} from "ethers"
+import Web3modal from 'web3modal'
+import {L3PAD_DAO_CONTRACT,L3PAD_DAO_ABI,CRYPTO_DEV_NFT_ABI,CRYPTO_DEV_NFT_CONTRACT} from "../constants"
+
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false)
+  const [loading,setLoading] = useState(false);
+  const web3ModalRef = useRef();
+  const [treasuryBalance, setTreasuryBalance] = useState("0")
+  const [nftBalance,setNftBalance] = useState(0)
+  const [numProposals, setNumProposals] = useState("0")
+  const [fakeNftTokenId, setFakeNftTokenId] = useState(" ");
+  const [proposals, setProposals] = useState([]);
+
+  // HELPER function to get the instance of the DAO contract
+  const getDAOContractInstance = (providerOrSigner) =>{
+    return new Contract(
+      L3PAD_DAO_CONTRACT,
+      L3PAD_DAO_ABI,
+      providerOrSigner
+    )
+  }
+  // HELPER function to get the instance of the Crypto devs NFT contract
+  const getCryptoDevNFTContractInstance = (providerOrSigner)=>{
+    return new Contract(
+      CRYPTO_DEV_NFT_CONTRACT,
+      CRYPTO_DEV_NFT_ABI,
+      providerOrSigner
+    )
+  };
+
+
+  // reads the remaining balnce of the DAO contract and sets the 'treasuryBalance variable'
+  const getDAOTreasuryBalance =async () =>{
+    try {
+      const provider = await getProviderOrSigner();
+      const balance = await provider.getBalance(L3PAD_DAO_CONTRACT);
+      setTreasuryBalance(balance.toString())
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // reads the balance of the crypto devs nft of the connected wallet and set the 'nftBalance variable'
+  const getNFTBalance = async()=>{
+    try {
+      const signer = await getProviderOrSigner(true)
+      const nftContract = getCryptoDevNFTContractInstance(signer);
+      const balance = await nftContract.balanceOf(signer.getAddress());
+      setNftBalance(parseInt(balance.toString()));
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  // gets the number of proposals of the DAO contract and set the 'numProposals variable'
+  const getNumProposalsInDAO = async() =>{
+    try {
+      const provider = await getProviderOrSigner();
+      const daoContract = getDAOContractInstance(provider);
+      const _numProposals = await daoContract.numProposals();
+      setNumProposals(_numProposals.toString());
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // calls 'createProposal()' from the contract using the tokenId from the fakeNftTokenId
+  const createProposal = async()=>{
+    try {
+      const signer = await getProviderOrSigner(true);
+      const daoContract = getDAOContractInstance(signer);
+      const txn = await daoContract.createProposal(fakeNftTokenId);
+      setLoading(true)
+      await txn.wait()
+      await getNumProposalsInDAO();
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      window.alert(error.message)
+    }
+  }
+
+  // Helper function to fetch and parse one proposal from the dao contract given the id, converts the returned data
+  // into js objects
+
+  const fetchProposalById = async(id) =>{
+    try {
+      const provider = await getProviderOrSigner();
+      const daoContract = getDAOContractInstance(provider);
+      const proposal = await daoContract.proposals(id);
+      const parsedProposal = {
+        proposalId : id,
+        nftTokenId : proposal.nftTokenId.toString(),
+        deadline : new Date(parseInt(proposal.deadline.toString())*1000),
+        yayVotes: proposal.yayVotes.toString(),
+        nayVotes: proposal.nayVotes.toString(),
+        executed: proposal.executed
+      }
+      return parsedProposal;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // looping through the proposals to set the 'proposal' variables
+  const fetchAllProposals = async()=>{
+    try {
+      const proposals = []
+      for(let i=0;i<numProposals;i++){
+        const proposal = fetchProposalById(i);
+        proposals.push(proposal)
+      }
+      setProposals(proposals)
+      return proposals
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getProviderOrSigner = async(needSigner=false) =>{
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const {chainId} = await web3Provider.getNetwork();
+    if(chainId!==5){
+      window.alert("Please choose Goerli network");
+      throw new Error("Please choose Goerli network");
+    }
+    if (needSigner){
+      const signer = web3Provider.getSigner();
+      return signer
+    }
+    return web3Provider
+  }
+
+  const connectWallet = async() =>{
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    if(!walletConnected){
+      web3ModalRef.current = new Web3modal(
+        {
+          network:goerli,
+          providerOptions: {},
+          disableInjectedProvider: false
+        }
+      )
+    }
+    connectWallet().then(()=>{
+      getDAOTreasuryBalance(),
+      getNFTBalance(),
+      getNumProposalsInDAO()
+    })
+  },[walletConnected])
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>L3pad Onchain DAO </title>
+        <meta name="description" content="DAO app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
